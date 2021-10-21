86f8857d3cab611146d82b00bee5672f62deba79
bloodredhood <3drusher@gmail.com>
2021-10-21 19:06:09 +0300

    creating and filling db.json

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import {render} from 'react-dom'
+import App from './App'
+import {BrowserRouter} from 'react-router-dom'
+
+render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
